9b25a85fc68b3337f2ed0937aaae8150ab5b9bf8
K4nishk <k4nishk@github.com>
2021-09-23 20:51:25 -0600

    final tweaks

--- login.py
+++ login.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import cgi, cgitb
+import os, cgi, cgitb
 from templates import secret_page
 import secret
 import time
@@ -13,6 +13,14 @@ password = form.getvalue('password')
 
 print("Content-Type: text/html\r\n\r\n")
 # print("<p><b>Username</b> %s <b>password</b> %s</p>" % (username, password))
-if secret.username == username and secret.password == password:
-    print("<p>Set-Cookie: lastvisit=" + str(time.time()) + "</p>")
-    print(secret_page(username, password))
+# if secret.username == username and secret.password == password:
+#     print("<p>Set-Cookie: lastvisit=" + str(time.time()) + "</p>")
+#     print(secret_page(username, password))
+
+# print(os.environ["HTTP_COOKIE"])
+cookies = os.environ["HTTP_COOKIE"].split(";")
+uname = cookies[0].strip().split("=")[1]
+pwd = cookies[1].strip().split("=")[1]
+
+if secret.username == uname and secret.password == pwd:
+    print(secret_page(uname, pwd))
